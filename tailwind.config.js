795c18d4d4c79b99cefaa73bacae082844e2c3b3
dexsteroxy <dexsteroxy.1234@gmail.com>
2022-11-13 11:46:25 -0800

    bento

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors:{
+        //  lightblack: '#162D59',
+        lightblack: 'rgb(4, 14,48)',
+    black:  '#081536',
+    skyblue: 'rgb(8, 203, 241)',
+    orange: '#F27405',
+    gray: '#C4C4C4',
+    // gray: 'rgb(199, 197, 197)'
+      },
+    },
+  },
+  plugins: [],
+}
